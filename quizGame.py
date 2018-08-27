3f2013c8e7c39c6820ed54373721519423579b9b
siddheshmurudkar <siddhesh@curiositygym.com>
2018-08-27 18:19:00 +0530

    added answer lock feature

--- quizGame.py
+++ quizGame.py
@@ -76,20 +76,20 @@ quizQuestions['questionBank'].append({
 
 def answerInput():
     text = ""
-    try:
-        t0 = time.clock()
-        while(time.clock()-t0 > 10):
+    t0 = time.clock()
+    while(time.clock()-t0 > 10):
+        try:
             id, text = reader.read()
             print(id)
             print(text)
-        #if (time.clock() -t0 > 15):
-         #   text ='na'
-    #except IndexError as e:
-     #   print(e)
-     #   engine.say("problem in reading your tag, please enter your answer again")
-     #   engine.runAndWait()
-    finally:
-        GPIO.cleanup()
+            #if (time.clock() -t0 > 15):
+             #   text ='na'
+        #except IndexError as e:
+         #   print(e)
+         #   engine.say("problem in reading your tag, please enter your answer again")
+         #   engine.runAndWait()
+        finally:
+            GPIO.cleanup()
         
     return text
 
